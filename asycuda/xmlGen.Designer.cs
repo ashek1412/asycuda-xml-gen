6632b2dad69bfcadee795054e3b263ffde23f225
Mahmudul Hasan <ashek1412@gmail.com>
2022-01-06 14:43:23 +0600

    Initial commit

---
+++ asycuda/xmlGen.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace asycuda
+{
+    partial class xmlGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.buttonUpd = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxFlt = new System.Windows.Forms.TextBox();
+            this.textBoxMAWB = new System.Windows.Forms.TextBox();
+            this.textBoxOC = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.buttonXml = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(291, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Excel :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(369, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(386, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(771, 12);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 2;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // buttonUpd
+            // 
+            this.buttonUpd.Enabled = false;
+            this.buttonUpd.Location = new System.Drawing.Point(852, 12);
+            this.buttonUpd.Name = "buttonUpd";
+            this.buttonUpd.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpd.TabIndex = 3;
+            this.buttonUpd.Text = "Upload";
+            this.buttonUpd.UseVisualStyleBackColor = true;
+            this.buttonUpd.Click += new System.EventHandler(this.buttonUpd_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 192);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1211, 495);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(363, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Customs Office Code :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(578, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Flight Number :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(389, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Departure Date:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(591, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "MAWB No.:";
+            // 
+            // textBoxFlt
+            // 
+            this.textBoxFlt.Location = new System.Drawing.Point(657, 47);
+            this.textBoxFlt.Name = "textBoxFlt";
+            this.textBoxFlt.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFlt.TabIndex = 9;
+            // 
+            // textBoxMAWB
+            // 
+            this.textBoxMAWB.Location = new System.Drawing.Point(657, 81);
+            this.textBoxMAWB.Name = "textBoxMAWB";
+            this.textBoxMAWB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMAWB.TabIndex = 10;
+            // 
+            // textBoxOC
+            // 
+            this.textBoxOC.Location = new System.Drawing.Point(472, 47);
+            this.textBoxOC.Name = "textBoxOC";
+            this.textBoxOC.Size = new System.Drawing.Size(37, 20);
+            this.textBoxOC.TabIndex = 11;
+            this.textBoxOC.Text = "101";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(472, 81);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(98, 20);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // buttonXml
+            // 
+            this.buttonXml.Enabled = false;
+            this.buttonXml.Location = new System.Drawing.Point(552, 117);
+            this.buttonXml.Name = "buttonXml";
+            this.buttonXml.Size = new System.Drawing.Size(114, 28);
+            this.buttonXml.TabIndex = 13;
+            this.buttonXml.Text = "Generate XML";
+            this.buttonXml.UseVisualStyleBackColor = true;
+            this.buttonXml.Click += new System.EventHandler(this.buttonXml_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.buttonXml);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.buttonBrowse);
+            this.panel1.Controls.Add(this.textBoxOC);
+            this.panel1.Controls.Add(this.textBoxMAWB);
+            this.panel1.Controls.Add(this.textBoxFlt);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.buttonUpd);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1211, 174);
+            this.panel1.TabIndex = 14;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::asycuda.Properties.Resources.bab;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 67);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // xmlGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1235, 699);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "xmlGen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Babylon Express - Generate XML";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Button buttonUpd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxFlt;
+        private System.Windows.Forms.TextBox textBoxMAWB;
+        private System.Windows.Forms.TextBox textBoxOC;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button buttonXml;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ asycuda/xmlGen.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace asycuda
+{
+    public partial class xmlGen : Form
+    {
+        public xmlGen()
+        {
+            InitializeComponent();
+           // Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+           // Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+           // Microsoft.Office.Interop.Excel.Range range;
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Excel Files";
+
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            openFileDialog1.DefaultExt = "xlsx";
+
+         //   openFileDialog1.Filter = "Text files (*. OpenFileDialog openFileDialog1 = new OpenFileDialog()";
+
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Browse Text Files";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+
+            openFileDialog1.DefaultExt = "xlsx";
+            openFileDialog1.Filter = "excel files (*.xlsx)|*.xlsx";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                textBox1.Text = openFileDialog1.FileName;
+                buttonUpd.Enabled = true;
+
+            }
+        }
+
+        private void buttonUpd_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                System.Data.OleDb.OleDbConnection MyConnection;
+                System.Data.DataSet DtSet;
+                System.Data.OleDb.OleDbDataAdapter MyCommand;
+                MyConnection = new System.Data.OleDb.OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source='" + textBox1.Text + "';Extended Properties=Excel 12.0;");
+                MyCommand = new System.Data.OleDb.OleDbDataAdapter("select * from [Sheet1$]", MyConnection);
+                MyCommand.TableMappings.Add("Table", "Net-informations.com");
+                DtSet = new System.Data.DataSet();
+                MyCommand.Fill(DtSet);
+                dataGridView1.DataSource = DtSet.Tables[0];
+                MyConnection.Close();
+
+
+
+
+                if (dataGridView1.RowCount > 0 && validateExcel() == 1)
+                    buttonXml.Enabled = true;
+                else
+                {
+                    buttonXml.Enabled = false;
+                    MessageBox.Show("Shipment data missing in excel file. Please Chcek !");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                string error = ex.Message;
+            }
+
+
+
+        }
+
+        private void buttonXml_Click(object sender, EventArgs e)
+        {
+            if (textBoxFlt.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Enter Flight Number");
+                return ;
+            }
+            if (textBoxMAWB.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Enter Master Airway Bill Number");
+                return;
+            }
+            if (textBoxMAWB.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Enter Master Airway Bill Number");
+                return;
+            }
+
+            string xml_name = "DEG101149007_" + DateTime.Now.ToString("dd") + DateTime.Now.ToString("MM") + DateTime.Now.ToString("yy") + DateTime.Now.ToString("HH")+ DateTime.Now.ToString("mm") + DateTime.Now.ToString("ss")+".xml";
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @"C:\";
+            saveFileDialog1.Title = "Save XML Files";
+            saveFileDialog1.CheckFileExists = false;
+            saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.OverwritePrompt = true;
+            saveFileDialog1.CreatePrompt = false;
+            saveFileDialog1.DefaultExt = "xml";
+            saveFileDialog1.Filter = "XML files (*.xml)|*.xml";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            saveFileDialog1.FileName = xml_name;
+
+           // XmlDocument doc = new XmlDocument();
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                XmlTextWriter writer = new XmlTextWriter(saveFileDialog1.FileName, System.Text.Encoding.UTF8);
+            writer.WriteStartDocument(true);
+            writer.Formatting = Formatting.Indented;
+            writer.Indentation = 2;
+            writer.WriteStartElement("Awbolds");
+            writer.WriteStartElement("Master_bol");
+            writer.WriteElementString("Customs_office_code", textBoxOC.Text.ToString());
+            writer.WriteElementString("Voyage_number", textBoxFlt.Text.ToString());
+            writer.WriteElementString("Date_of_departure", dateTimePicker1.Text.ToString());
+            writer.WriteElementString("Reference_number", textBoxMAWB.Text.ToString());
+            writer.WriteEndElement();
+
+           
+
+            //following will iterate all rows
+            int Line_number = 1;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                string Bol_reference = xml_entities(row.Cells[1].Value.ToString());
+                string Goods_description = xml_entities(row.Cells[2].Value.ToString());
+                string Port_of_origin_code = xml_entities(row.Cells[3].Value.ToString());
+                string Carrier_code = xml_entities(row.Cells[4].Value.ToString());
+                string Carrier_name = xml_entities(row.Cells[5].Value.ToString());
+                string Carrier_address = xml_entities(row.Cells[6].Value.ToString());
+                string Exporter_name = xml_entities(row.Cells[7].Value.ToString());
+                string Exporter_address = xml_entities(row.Cells[8].Value.ToString());
+                string Notify_code = xml_entities(row.Cells[9].Value.ToString());
+                string Notify_name = xml_entities(row.Cells[10].Value.ToString());
+                string Notify_address = xml_entities(row.Cells[11].Value.ToString());
+                string Consignee_code = xml_entities(row.Cells[12].Value.ToString());
+                string Consignee_name = xml_entities(row.Cells[13].Value.ToString());
+                string Consignee_address = xml_entities(row.Cells[14].Value.ToString());
+                int Number_of_packages =Int32.Parse(row.Cells[15].Value.ToString());
+                string Package_type_code = xml_entities(row.Cells[16].Value.ToString());
+                decimal Gross_mass = Math.Round(Decimal.Parse(row.Cells[17].Value.ToString()),2);
+                string Shipping_marks = xml_entities(row.Cells[18].Value.ToString());
+
+                writer.WriteStartElement("Bol_segment");
+                    writer.WriteStartElement("Bol_id");
+                        writer.WriteElementString("Bol_reference", Bol_reference);
+                        writer.WriteElementString("Line_number", Line_number.ToString());
+                        writer.WriteElementString("Bol_nature", "23");
+                        writer.WriteElementString("Bol_type_code", "AWB");
+                        writer.WriteElementString("Master_bol_ref_number", textBoxMAWB.Text.ToString());
+                        writer.WriteElementString("DG_status", "");
+                    writer.WriteEndElement();
+
+                         writer.WriteElementString("Consolidated_Cargo", "0");
+
+                    writer.WriteStartElement("Load_unload_place");
+                        writer.WriteElementString("Port_of_origin_code", Port_of_origin_code);
+                        writer.WriteElementString("Place_of_unloading_code", "BDDAC");
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("Traders_segment");
+                        writer.WriteStartElement("Carrier");
+                              writer.WriteElementString("Carrier_code", Carrier_code);
+                              writer.WriteElementString("Carrier_name", Carrier_name);
+                              writer.WriteElementString("Carrier_address", Carrier_address);
+                        writer.WriteEndElement();
+                        writer.WriteStartElement("Shipping_Agent");
+                                 writer.WriteElementString("Shipping_Agent_code", "");
+                                 writer.WriteElementString("Shipping_Agent_name", "");
+                        writer.WriteEndElement();
+                        writer.WriteStartElement("Exporter");
+                            writer.WriteElementString("Exporter_name", Exporter_name);
+                            writer.WriteElementString("Exporter_address", Exporter_address);
+                        writer.WriteEndElement();
+                        writer.WriteStartElement("Notify");
+                            writer.WriteElementString("Notify_code", "");
+                            writer.WriteElementString("Notify_name", Notify_name);
+                            writer.WriteElementString("Notify_address", Notify_address);
+                       writer.WriteEndElement();
+                       writer.WriteStartElement("Consignee");
+                            writer.WriteElementString("Consignee_code","");
+                            writer.WriteElementString("Consignee_name", Consignee_code);
+                            writer.WriteElementString("Consignee_address", Consignee_address);
+                       writer.WriteEndElement();
+                  writer.WriteEndElement();
+
+                  writer.WriteStartElement("Goods_segment");
+                        writer.WriteElementString("Number_of_packages", "");
+                        writer.WriteElementString("Package_type_code", Package_type_code);
+                        writer.WriteElementString("Gross_mass", Gross_mass.ToString());
+                        writer.WriteElementString("Shipping_marks", Shipping_marks);
+                        writer.WriteElementString("Goods_description", Goods_description);
+                        writer.WriteElementString("Volume_in_cubic_meters", "0.0");
+                        writer.WriteElementString("Num_of_ctn_for_this_bol", "0");
+                        writer.WriteElementString("Remarks", "");
+                 writer.WriteEndElement();
+
+                  writer.WriteStartElement("Value_segment");
+                        writer.WriteStartElement("Freight_segment");
+                            writer.WriteElementString("PC_indicator", "");
+                            writer.WriteElementString("Freight_value", "0.0");
+                            writer.WriteElementString("Freight_currency","ZZZ");
+                        writer.WriteEndElement();
+                        writer.WriteStartElement("Customs_segment");
+                            writer.WriteElementString("Customs_value", "0.0");
+                            writer.WriteElementString("Customs_currency", "");
+                        writer.WriteEndElement();
+                writer.WriteEndElement();
+
+
+
+                writer.WriteEndElement();
+
+
+
+                Line_number++;
+            }
+
+            writer.WriteEndElement();
+
+            writer.WriteEndDocument();
+            writer.Close();
+
+             MessageBox.Show("XML File created ! ");
+
+
+
+            }
+
+
+        }
+
+
+        private String xml_entities(String st)
+        {
+            String st1 = null;
+            st1 = Regex.Replace(st, @"/[\x00-\x1F\x7f-\xFF]/", " ");
+            st1 = Regex.Replace(st1,@"/[^A-Za-z0-9 _\-\+\&]/", " ");
+            st1 = Regex.Replace(st1.Trim(),@"/[ ]{2,}|[\t]/", " " );
+
+            return st1.Replace("<", " ").Replace(">", " ").Replace("\"", " ").Replace("'", " ").Replace("&", " and ").Replace("/", " ")
+                        .Replace("\\", " ").Replace("%", " ").Replace("\\", " ").Replace("?", " ").Replace("`", " ").Replace("@", " ");
+
+                    
+
+        }
+
+
+
+        private int validateExcel()
+        {
+
+
+            int isok = 1;
+            int rok = 1;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                rok = 1;
+                foreach (DataGridViewCell cl in row.Cells)
+                {
+                    if (string.IsNullOrEmpty(cl.Value.ToString()) && cl.ColumnIndex != 9 && cl.ColumnIndex != 12)
+                    {
+                        cl.Style.BackColor = System.Drawing.Color.Pink;
+                        isok = 0;
+                        rok = 0;
+
+                    }
+                }
+                if (rok == 0)
+                    row.Cells[0].Style.BackColor = System.Drawing.Color.Yellow;
+                else
+                    row.Cells[0].Style.BackColor = System.Drawing.Color.White;
+            }
+
+            return isok;
+
+
+
+        }
+
+    }
+
+
+}
